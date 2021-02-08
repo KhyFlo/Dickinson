2ad2a3cb6f0bd58ad23c673ba28a3847834cd3ca
KhyFlo <78616080+KhyFlo@users.noreply.github.com>
2021-02-08 13:14:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+<body>
+    <div class="header">
+        <div class="logo"><img src="logo.png" /></div>
+    </div>
+<div class="content">
+
 const students = [
     {
       name: "Sarah Sheriff",
@@ -13,7 +19,37 @@ const students = [
       name: "Whitney Long-Maisano",
       hometown: "Mechanicsburg",
       major: "Human Resource Management",
-    }
+    },
+    {
+      name: "Sam Smith",
+      hometown: "Allentown",
+      major: "Marketing",
+    }, 
+    {
+      name: "Greg Platzer",
+      hometown: "Nashville",
+      major: "Business Administration",
+    }, 
+    {
+      name: "Mike Stamp",
+      hometown: "Chicago",
+      major: "Healthcare Administration",
+    }, 
+    {
+      name: "Jennifer Burke",
+      hometown: "Dallas",
+      major: "Liberal Arts",
+    }, 
+    {
+      name: "Cynthia Hyde",
+      hometown: "Roanoke",
+      major: "Psychology",
+    }, 
+    {
+      name: "Justin Flores",
+      hometown: "Los Angeles",
+      major: "Sports Medicine",
+    }, 
   ];
   
   new Vue ({
