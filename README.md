7e9f09ce423fd24c7b5b859d9dbc62eabd5078f6
KhyFlo <78616080+KhyFlo@users.noreply.github.com>
2021-02-08 13:03:17 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Code-Challenge
+Online Marketing Coding Challenge
+
+This is for candidates for the Assistant Director of Online Marketing position. Instructions can be found in the index.html file. 
---
+++ main.js
@@ -0,0 +1,26 @@
+const students = [
+    {
+      name: "Sarah Sheriff",
+      hometown: "Nashville",
+      major: "Digital Communications",
+    },
+    {
+      name: "Greg Platzer",
+      hometown: "Nashville",
+      major: "Business Administration",
+    },  
+    {
+      name: "Whitney Long-Maisano",
+      hometown: "Mechanicsburg",
+      major: "Human Resource Management",
+    }
+  ];
+  
+  new Vue ({
+    el: '#app',
+    data: {
+      
+      
+      
+    }
+  });
